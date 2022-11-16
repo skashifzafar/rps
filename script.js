9aa56c047c33fe28cb4e7f103276b9bb1056f2cf
skz <zafar.syedkashif@gmail.com>
2022-11-16 23:53:06 +0530

    base game

--- script.js
+++ script.js
@@ -2,38 +2,50 @@ function getComputerChoice(){
     var ran_arr=["rock","paper","scissor"];
     return ran_arr[(Math.random()*ran_arr.length)|0]
 }
-function game(comp,user){
+function playRound(comp,user){
     if (user==comp){
-        return "Draw! Nobody wins."
+        return 2
     }
     switch(user){
         case "rock":
             if (comp==="paper"){
-                return "Computer wins!"
+                return 0
             }
             if (comp==="scissor"){
-                return "You win!"
+                return 1
             }
         case "paper":
             if (comp==="scissor"){
-                return "Computer wins!"
+                return 0
             }
             if (comp==="rock"){
-                return "You win!"
+                return 1
             }
         case "scissor":
             if (comp==="rock"){
-                return "Computer wins!"
+                return 0
             }
             if (comp==="paper"){
-                return "You win!"
+                return 1
             }
     }
 
 }
-var in_arr=["rock","paper","scissor","paper","scissor"];
+function game(in_arr){
 var com
-for (i=0;i<5;i++){
+var c=0, u=0
+for (i=0;i<in_arr.length;i++){
     com=getComputerChoice()
-    console.log(game(com,in_arr[i]),com,in_arr[i])    
+    var test=playRound(com,in_arr[i])
+    if (test==0){
+        c++
+    }
+    else if(test==1){
+        u++
+    }
+
+    console.log(test,com,in_arr[i],c,u)    
 }
+}
+var in_arr=["rock","paper","scissor","paper","scissor","rock"];
+game(in_arr)
